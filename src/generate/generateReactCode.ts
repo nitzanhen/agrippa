2087d0d004106f8bb04b52923223b39737786018
NitzanHen <nitzan.alpha@gmail.com>
2021-08-07 18:43:19 +0000

    Add jsdoc & prop-types validation

--- src/generate/generateReactCode.ts
+++ src/generate/generateReactCode.ts
@@ -12,23 +12,35 @@ export function generateReactCode({ props, importReact, name, typescript, childr
   const typeClass = children ? 'FC' : 'VFC';
   const TSProps = typescript && props === 'ts'
 
-  const componentType = ostr(TSProps, `React.${typeClass}<${interfaceName}>`)
+  const componentType = `React.${typeClass}<${interfaceName}>`
 
   const createStylesFile = (styling === 'css' || styling === 'scss');
 
   return [
+    line(0, ostr(props === 'jsdoc', '//@ts-check')),
     line(0, ostr(importReact, "import React from 'react';")),
     line(0, ostr(createStylesFile && stylingModule, `import classes from './${kcName}.module.${styling}'`)),
     line(0, ostr(createStylesFile && !stylingModule, `import './${kcName}.${styling}'`)),
     line(0, ostr(styling === 'jss', "import { createUseStyles } from 'react-jss';")),
     line(0, ostr(styling === 'mui', "import { makeStyles } from '@material-ui/core';")),
+    line(0, ostr(props === 'prop-types', "import PropTypes from 'prop-types'")),
     '',
     line(0, ostr(styling === 'jss', 'const useStyles = createUseStyles({});')),
     line(0, ostr(styling === 'mui', `const useStyles = makeStyles((theme${cstr(typescript, ': Theme')}) => {});`)),
     line(0, ostr(styling === 'jss' || styling === 'mui', '')),
+
     line(0, ostr(TSProps, `export interface ${pcName}Props {};`)),
     line(0, ostr(TSProps, '')),
-    line(0, `export const ${pcName}${cstr(!!componentType, `: ${componentType}`)} = (${cstr(children, '{ children }')}) => {`),
+
+    line(0, ostr(props === 'jsdoc', '/**')),
+    line(0, ostr(props === 'jsdoc', ` * @typedef ${interfaceName}`)),
+    line(0, ostr(props === 'jsdoc', ' */')),
+
+    line(0, ostr(props === 'jsdoc', '/**')),
+    line(0, ostr(props === 'jsdoc', ` * @type {${componentType}}`)),
+    line(0, ostr(props === 'jsdoc', ' */')),
+
+    line(0, `export const ${pcName}${cstr(TSProps, `: ${componentType}`)} = (${cstr(children, '{ children }')}) => {`),
     '',
     line(1, ostr(styling === 'jss' || styling === 'mui', 'const classes = useStyles();')),
     line(1, ostr(styling === 'jss' || styling === 'mui', '')),
@@ -37,5 +49,7 @@ export function generateReactCode({ props, importReact, name, typescript, childr
     line(2, `${children ? '<div>{children}</div>' : '<div />'}`),
     line(1, ');'),
     line(0, '}'),
+    line(0, ostr(props === 'prop-types', '')),
+    line(0, ostr(props === 'prop-types', `${pcName}.propTypes = {}`))
   ].filter(line => typeof line === 'string').join('\n');
 }
