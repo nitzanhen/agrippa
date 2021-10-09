3a19ebdbba437a26bcb267c6c52a83ecb35ba281
NitzanHen <nitzan.alpha@gmail.com>
2021-10-09 15:38:13 +0000

    test(codegenUtils): add codegenUtils tests.

--- test/unit/codegenUtils.test.ts
+++ test/unit/codegenUtils.test.ts
@@ -1,10 +1,102 @@
-import { createImport } from 'utils/codegenUtils';
+import { createArrowFunction, createAssignment, createComment, createDefaultExport, createImport, declareConst, declareFunction, declareInterface } from 'utils/codegenUtils';
 
 describe('utils/codegenUtils.ts', () => {
 
   test('createImport', () => {
-    expect(createImport('styles.css')).toEqual("import 'styles.css'");
+    expect(createImport('styles.css')).toBe("import 'styles.css';");
+    expect(createImport('module')).toBe("import 'module';");
+
+    expect(createImport('react', 'React', 'default')).toBe("import React from 'react';");
+    expect(createImport('jsonwebtoken', 'jwt', 'default')).toBe("import jwt from 'jsonwebtoken';");
+
+    expect(createImport('react', 'useMemo', 'named')).toBe("import { useMemo } from 'react';");
+    expect(createImport('@material-ui/core', 'makeStyles', 'named')).toBe("import { makeStyles } from '@material-ui/core';");
   });
 
+  test('declareConst', () => {
+    expect(declareConst('classes', 'useStyles()')).toBe('const classes = useStyles();');
+    expect(declareConst('computed', 'useMemo(() => 3)')).toBe('const computed = useMemo(() => 3);');
 
+    expect(declareConst('classes', 'useStyles()', true)).toBe('export const classes = useStyles();');
+    expect(declareConst('computed', 'useMemo(() => 3)', true)).toBe('export const computed = useMemo(() => 3);');
+
+    expect(declareConst('classes', 'useStyles()', false, 'Classes')).toBe('const classes: Classes = useStyles();');
+    expect(declareConst('computed', 'useMemo(() => 3)', false, 'number')).toBe('const computed: number = useMemo(() => 3);');
+
+    expect(declareConst('classes', 'useStyles()', true, 'Classes')).toBe('export const classes: Classes = useStyles();');
+    expect(declareConst('computed', 'useMemo(() => 3)', true, 'number')).toBe('export const computed: number = useMemo(() => 3);');
+  });
+
+  test('declareInterface', () => {
+    expect(declareInterface('ButtonProps')).toBe('interface ButtonProps {}');
+    expect(declareInterface('TabProps')).toBe('interface TabProps {}');
+
+    expect(declareInterface('ButtonProps', true)).toBe('export interface ButtonProps {}');
+    expect(declareInterface('TabProps', true)).toBe('export interface TabProps {}');
+
+  });
+
+  test('declareFunction', () => {
+    expect(declareFunction('Button')).toBe('function Button() { }');
+
+    expect(declareFunction('Button', 'props: ButtonProps')).toBe('function Button(props: ButtonProps) { }');
+
+    expect(declareFunction(
+      'Button',
+      '{ onClick }: ButtonProps',
+      'return <button onClick={onClick} />'
+    )).toBe('function Button({ onClick }: ButtonProps) {\n\treturn <button onClick={onClick} />\n}');
+
+    expect(declareFunction(
+      'Button',
+      '{ onClick }: ButtonProps',
+      'return <button onClick={onClick} />',
+      true
+    )).toBe('export function Button({ onClick }: ButtonProps) {\n\treturn <button onClick={onClick} />\n}');
+
+
+    expect(declareFunction('Tab')).toBe('function Tab() { }');
+
+    expect(declareFunction('Tab', '{ index }: TabProps')).toBe('function Tab({ index }: TabProps) { }');
+
+    expect(declareFunction('Tab', '{ index }: TabProps', 'console.log(index);')).toBe('function Tab({ index }: TabProps) {\n\tconsole.log(index);\n}');
+
+    expect(declareFunction(
+      'Tab',
+      '{ index }: TabProps',
+      'console.log(index);',
+      true
+    )).toBe('export function Tab({ index }: TabProps) {\n\tconsole.log(index);\n}');
+
+  });
+
+  test('createArrowFunction', () => {
+    expect(createArrowFunction()).toBe('() => {}');
+
+    expect(createArrowFunction('param: number')).toBe('(param: number) => {}');
+
+    expect(createArrowFunction('param: number', 'console.log(param);')).toBe('(param: number) => {\n\tconsole.log(param);\n}');
+    expect(createArrowFunction('{ cb: () => void }', 'cb();')).toBe('({ cb: () => void }) => {\n\tcb();\n}');
+  });
+
+  test('createDefaultExport', () => {
+    expect(createDefaultExport('Component')).toBe('export default Component;');
+    expect(createDefaultExport('memo(Component)')).toBe('export default memo(Component);');
+  });
+
+  test('createComment', () => {
+    expect(createComment('single', 'hello')).toBe('// hello');
+    expect(createComment('single', 'this is a comment')).toBe('// this is a comment');
+
+    expect(createComment('multi', 'hello')).toBe('/*\n * hello\n */');
+    expect(createComment('multi', 'this is a comment')).toBe('/*\n * this is a comment\n */');
+
+    expect(createComment('jsdoc', 'hello')).toBe('/**\n * hello\n */');
+    expect(createComment('jsdoc', 'this is a comment')).toBe('/**\n * this is a comment\n */');
+  });
+
+  test('createAssignment', () => {
+    expect(createAssignment('obj', 'a', '3')).toBe('obj.a = 3;');
+    expect(createAssignment('Component', 'propTypes', '{}')).toBe('Component.propTypes = {};');
+  });
 });
