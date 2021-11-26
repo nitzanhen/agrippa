e531fafd7d0dad5c0fad24d7e4210b795a0f7b1b
Nitzan Hen <code@nitzanhen.net>
2021-11-26 23:47:02 +0000

    add button-scss-ts integration test

---
+++ test/integration/button-scss-ts/solution/ScssTsButton/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './scss-ts-button.module.scss';
+
+export interface ScssTsButtonProps {}
+
+export const ScssTsButton: React.VFC<ScssTsButtonProps> = () => {
+	
+	return (
+		<div></div>
+	);
+};
