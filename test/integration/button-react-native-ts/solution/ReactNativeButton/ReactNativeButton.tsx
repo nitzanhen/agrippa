c67ff4c706ad780d6c844c7c8dd2943350562388
Nitzan Hen <code@nitzanhen.net>
2021-12-03 14:25:13 +0000

    add button-react-native-ts test

---
+++ test/integration/button-react-native-ts/solution/ReactNativeButton/ReactNativeButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+export interface ReactNativeButtonProps {}
+
+export const ReactNativeButton: React.VFC<ReactNativeButtonProps> = () => {
+	
+	return (
+		<View></View>
+	);
+};
