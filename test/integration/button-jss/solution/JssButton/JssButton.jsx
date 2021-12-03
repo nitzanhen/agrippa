743e1c0083cb1e8c119d7016d8616cbe45400735
Nitzan Hen <code@nitzanhen.net>
2021-12-03 14:37:18 +0000

    Add button-jss test

---
+++ test/integration/button-jss/solution/JssButton/JssButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({});
+
+export const JssButton = () => {
+	
+	const classes = useStyles();
+	
+	return (
+		<div></div>
+	);
+};
