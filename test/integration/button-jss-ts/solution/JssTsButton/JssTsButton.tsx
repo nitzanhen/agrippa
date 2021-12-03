b785601efe08c92a05e6426b83bc0d3565140415
Nitzan Hen <code@nitzanhen.net>
2021-12-03 14:37:26 +0000

    add button-jss-ts test

---
+++ test/integration/button-jss-ts/solution/JssTsButton/JssTsButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { createUseStyles } from 'react-jss';
+
+const useStyles = createUseStyles({});
+
+export interface JssTsButtonProps {}
+
+export const JssTsButton: React.VFC<JssTsButtonProps> = () => {
+	
+	const classes = useStyles();
+	
+	return (
+		<div></div>
+	);
+};
