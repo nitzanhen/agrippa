b27f6ac46374c506af1df27846963420dcd6c671
Nitzan Hen <code@nitzanhen.net>
2022-04-14 17:02:19 +0000

    Fix DeepPartial type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -4,6 +4,6 @@ export type Falsy = undefined | null | false | 0 | '';
 /** Makes only the keys K of T optional */
 export type SemiPartial<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
-export type DeepPartial<T> = {
-  [K in keyof T]?: T[K] extends object ? DeepPartial<T[K]> : T[K]
-}
+export type DeepPartial<T> = T extends object ? {
+  [K in keyof T]?: DeepPartial<T[K]>
+} : T;
