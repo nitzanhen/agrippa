9843e54784efff1b37c94b3e415c98dbcb549801
Nitzan Hen <code@nitzanhen.net>
2022-04-28 01:52:29 +0300

    Correct config typescriptOptions default logic

--- src/config/createConfig.ts
+++ src/config/createConfig.ts
@@ -66,7 +66,7 @@ export function createConfig(input: InputConfig, envFiles: Record<string, any>):
     reactNativeOptions: environment === Environment.REACT_NATIVE ? {} : undefined,
 
     typescript: !!tsconfig,
-    typescriptOptions: input.typescript ? {
+    typescriptOptions: (!!tsconfig || input.typescript) ? {
       propDeclaration: 'interface'
     } : undefined,
 
