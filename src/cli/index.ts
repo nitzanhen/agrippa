f424ecd2ef6595178ca14fd58573697af9b4683d
Nitzan Hen <code@nitzanhen.net>
2022-06-06 04:30:41 +0300

    Revert yargs changes

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -3,18 +3,13 @@ import { hideBin } from 'yargs/helpers';
 import { generateCommand } from './generate';
 import { initCommand } from './init';
 
-// logger.info(`\nAgrippa v${pkgJson.version}`);
-
-const y = yargs();
-
 // Init yargs
-y
-  .command(generateCommand)
+yargs(hideBin(process.argv)).command(generateCommand)
   .command(initCommand)
   .recommendCommands()
   .strict()
   .demandCommand(1, 'Please specify a command')
-  .parseAsync(hideBin(process.argv))
+  .parseAsync()
   .catch(console.error);
 
 
